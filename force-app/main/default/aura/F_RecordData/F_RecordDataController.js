d630496c830a2b9ba3170b78bbfc57c9b5a201a7
Andres Perez <aperez@aperez-ltm2.internal.salesforce.com>
2018-06-26 12:22:34 -0400

    Something wrong... need to back up!

--- force-app/main/default/aura/F_RecordData/F_RecordDataController.js
+++ force-app/main/default/aura/F_RecordData/F_RecordDataController.js
@@ -1,5 +1,17 @@
 ({
-	recordUpdated : function(component, event, helper) {
-		
-	}
+    recordUpdated: function(component, event, helper) {
+        debugger;
+        var eventParams = event.getParams();
+        if (eventParams.changeType === "CHANGED") {
+            console.log("Record is changed");
+        } else if (eventParams.changeType === "LOADED") {
+            console.log("Record is loaded in the cache");
+        } else if (eventParams.changeType === "REMOVED") {
+            console.log("Record is deleted");
+        } else if (eventParams.changeType === "ERROR") {
+            console.log("There’s an error while loading, saving, or deleting the record");
+        } else {
+            alert("What changed type? [" + eventParams.changeType + "]");
+        }
+    }
 })
