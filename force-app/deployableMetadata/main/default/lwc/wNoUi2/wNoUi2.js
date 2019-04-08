430ef0e4420988fa92f87f2dcbc148e8af2f7793
Andres Perez <andresperezf@yahoo.com>
2019-04-07 21:04:04 -0400

    wNoUi2

---
+++ force-app/deployableMetadata/main/default/lwc/wNoUi2/wNoUi2.js
@@ -0,0 +1,42 @@
+import { LightningElement, api, track } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi'
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class WNoUi2 extends LightningElement {
+	@api recordId;
+	@track caseId;
+	@track caseError;
+	@track goToCaseBtnLabel;
+	@track spinnerShown = false;
+	@track caseRecord = {
+		Subject: "",
+		Origin: "",
+		Description: ""
+	};
+
+	subjectChanged(event) {
+		this.caseRecord.Subject = event.detail.value;
+	}
+
+	originChanged(event) {
+		this.caseRecord.Origin = event.detail.value;
+	}
+
+	descriptionChanged(event) {
+		this.caseRecord.Description = event.detail.value;
+	}
+
+	// constructor() {
+	// 	super();
+
+	// }
+
+	// <force:recordData aura:id="caseCreator" recordId="{!v.caseId}" layoutType="FULL" mode="EDIT"
+	// 				targetRecord="{!v.caseRecord}" targetFields="{!v.caseFields}" targetError="{!v.caseError}"
+	// 				recordUpdated="{!c.caseUpdated}" />
+
+
+	saveRecord(event) {
+		console.log(this.caseRecord)
+	}
+}
